140a7b93116744a1454352136b81d6435caf79b3
NDHau <nguyendinhhau1807@gmail.com>
2020-05-12 02:34:47 +0700

    Initial commit

---
+++ public/js/stripe.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+import axios from 'axios';
+import { showAlert } from './alerts';
+const stripe = Stripe('pk_test_Vkfk9i8WfEi6SNWSHiQjx9Sf00TVa7CuLz');
+
+export const bookTour = async (tourId) => {
+  try {
+    // 1) Get checkout session from APT
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    console.log(session);
+
+    // 2) Create checkout form + charge credit card
+    await stripe.redirectToCheckout({
+      sessionId: session.data.session.id,
+    });
+  } catch (err) {
+    console.log(err);
+    showAlert('error', err);
+  }
+};
